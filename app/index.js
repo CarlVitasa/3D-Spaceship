c71ece79a968b1d59ead886e75a6101f70d3ae54
Carl Vitasa <cvitasa@magicleap.com>
2018-08-26 01:11:05 -0400

    added model in scene

--- app/index.js
+++ app/index.js
@@ -2,27 +2,34 @@ import 'styles/index.scss';
 
 import {
     Vector2,
+    Vector3,
     WebGLRenderer,
     Scene,
     PerspectiveCamera,
     BoxGeometry,
     MeshBasicMaterial,
+    MeshStandardMaterial,
     Mesh,
     SphereGeometry,
-    Vector3,
-} from 'three';
+    ObjectLoader,
+    AmbientLight,
+} 
+from '../node_modules/three';
 
 const WIDTH = 640;
 const HEIGHT = 640;
-const MOUSE_OFFSET = 1.5;
+const MOUSE_OFFSET_X = 9;
+const MOUSE_OFFSET_Y = 9;
 const RATIO = WIDTH/HEIGHT;
 
 class App {
   constructor(){
+
     // renderer
     const canvas = document.getElementById("glCanvas");
     this.renderer = new WebGLRenderer({canvas: canvas});
     this.renderer.setSize(WIDTH, HEIGHT, false);
+    // this.renderer.setClearColor("#222222");
     document.body.appendChild(this.renderer.domElement);
 
     // scene
@@ -31,25 +38,32 @@ class App {
 
     // camera
     this.camera = new PerspectiveCamera(35, WIDTH / HEIGHT, 1, 10000);
-    this.camera.position.set(0, 0, 5);
-    
-    // cube
-    const geometry = new BoxGeometry(0.4, 0.1, 1);
-    const material = new MeshBasicMaterial({ color: 0x5f5f5f });
-    this.cube = new Mesh(geometry, material);
-    this.scene.add(this.cube);  
-    
+    this.camera.position.set(0, 0, 30);
+
+    // model
+    const objectLoader = new ObjectLoader();
+    objectLoader.load(
+      "./assets/models/json/model.json",
+      (obj) => { // TODO: says this.spaceship is undefined but is working
+        this.spaceship = obj;
+        this.scene.add(this.spaceship);
+      });
+
     // debug sphere
     const geoSphere = new SphereGeometry(0.1, 8);
     const matSphere = new MeshBasicMaterial({color: 0xff0000});
     this.meshSphere = new Mesh(geoSphere, matSphere);
     // scene.add(meshSphere);
     
+    // lighting
+    const ambientLight = new AmbientLight ( 0xffffff, 0.9)
+    this.scene.add( ambientLight );
+
     this.addEventListeners();
     this.animate();
     this.onResize();  
-  }
 
+  }
   addEventListeners = () => {
     this.renderer.domElement.addEventListener( 'mousemove', this.onMouseMove );
     window.addEventListener( 'resize', this.onResize );
@@ -85,32 +99,32 @@ class App {
     requestAnimationFrame(this.animate);
     this.renderer.render(this.scene, this.camera);
     
-    // this.cube.position.x = mouse.x*MOUSE_OFFSET;
-    // this.cube.position.y = mouse.y*MOUSE_OFFSET;
+    // this.spaceship.position.x = mouse.x*MOUSE_OFFSET;
+    // this.spaceship.position.y = mouse.y*MOUSE_OFFSET;
     
     
     const lerpTo = new Vector3(
-        this.mouse.x*MOUSE_OFFSET,
-        this.mouse.y*MOUSE_OFFSET,
-        this.cube.position.z,
+        this.mouse.x*MOUSE_OFFSET_X,
+        this.mouse.y*MOUSE_OFFSET_Y,
+        this.spaceship.position.z,
       );
     
-    this.cube.position.lerp(lerpTo, 0.2);
+    this.spaceship.position.lerp(lerpTo, 0.3);
     
     const sign = new Vector2(
-      this.cube.position.x,
-      this.cube.position.y)
+      this.spaceship.position.x,
+      this.spaceship.position.y)
     .dot(
       new Vector2(
         lerpTo.x, 
         lerpTo.y));  
     
-    const valX = this.cube.position.x - lerpTo.x;
-    const valY = this.cube.position.y - lerpTo.y;
+    const valX = this.spaceship.position.x - lerpTo.x;
+    const valY = this.spaceship.position.y - lerpTo.y;
    
-    this.cube.rotation.z = valX;
-    this.cube.rotation.x = -valY;
-    this.cube.rotation.y = valX;
+    this.spaceship.rotation.z = valX;
+    this.spaceship.rotation.x = -valY;
+    this.spaceship.rotation.y = valX;
     
     this.meshSphere.position.x = lerpTo.x;
     this.meshSphere.position.y = lerpTo.y;
